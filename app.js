9f9366a4c8d37d62a2cbde6e67638f17aa8b48af
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-02-03 12:46:02 -0500

    Added initial prompting and switches

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-const fs = require('fs')
+
+const fs = require('fs');
+const { start } = require("repl");
 
 const app = express();
 
@@ -18,6 +20,52 @@ const connection = mysql.createConnection({
     password: "Dubhghall1!",
     database: "company_db"
   });
+
+  function getAction(){
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: ["View Information", "Add New Information", "Exit"]
+        }
+    ]).then(data => {
+        if(data.action === "View Information"){
+            viewData();
+        }else if (data.action ==="Add New Information"){
+            addData();
+        }else if (data.action ==="Exit"){
+           connection.end(); 
+       
+        }else {
+            start();
+        };
+    });
+}
+ function viewData() {
+     inquirer.prompt([
+         {
+             type: "list",
+             name: "view",
+             message: "Please select what you would like to view:",
+             choices:["Departments", "Job Roles", "Employees", "Hit me with Everything"]
+         }   
+     ]).then (answer => {
+        switch (answer.view) {
+            case "Departments":
+                displayDepts();
+            case "Job Roles":
+                displayRoles();
+            case "Employees":
+                displayEmployees();
+            case "Hit me with Everything":
+                displayAll();
+            default :
+                viewData();
+        }
+     })
+ }
+
 
   connection.connect(function(err) {
     if (err) throw err;
